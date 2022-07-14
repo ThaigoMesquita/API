0b163fd057d204894811e6abbf29ae51ef183198
thiago <taigomesquita.tm@gmail.com>
2022-07-14 17:05:28 -0300

    api terminada

--- main.py
+++ main.py
@@ -1,17 +1,49 @@
-from optparse import Option
-from typing import Optional
-from fastapi import FastAPI
+from typing import Union
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+from random import randint
+
 
 app = FastAPI()
 
+banco_de_dados = []
+
+class Produto(BaseModel):
+    titulo: str
+    descricao: Union[str, None] = None
+    link: Union[str, None] = None
+    foto: Union[str, None] = None
+    recebido_como: Union[str, None] = None
 
 @app.get("/")
-async def root():
-    return {"WishList"}
+def read_root():
+    return "lista de desejo"
+
+@app.post("/produto")
+def adiciona_produto(produto: Produto = Body(embed=True)):
+    if len(banco_de_dados) != 0:
+        ultimo_produto = banco_de_dados[-1]
+        ultimo_id = ultimo_produto["id"]
+        ultimo_id = ultimo_id + 1
+        produto = dict(produto)
+        produto["id"] = ultimo_id
+        banco_de_dados.append(produto)
+        return banco_de_dados
+    
+    if len(banco_de_dados) == 0:
+        produto = dict(produto)
+        produto["id"] = 0
+        banco_de_dados.append(produto)
+        return banco_de_dados
 
 
-@app.get("/items/{item_id}")
-async def read_item(produto: str, descrição: Optional[str]= None,link: Optional[str]= None, foto: Optional[str]= None ):
-    return {"produto": produto, "link": link, "foto": foto,  "descrição":descrição  }
+@app.get ("/produto")
+def pega_produto_aleatorio():
+    tamanho_da_lista = len(banco_de_dados)
+    posicao = randint(0, tamanho_da_lista - 1)
+    return banco_de_dados[posicao]
 
 
+@app.get("/produtos")
+def pega_todos_os_produtos():
+    return banco_de_dados
